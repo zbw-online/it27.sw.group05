a64332f55f96c3f6448f605a784b6b12ca3ca6f3
dominicgrob123 <dominic.grob@hotmail.ch>
2026-02-01 11:30:56 +0100

    added IRepository

---
+++ src/SharedKernel/SeedWork/IRepository.cs
@@ -0,0 +1,14 @@
+using SharedKernel.Primitives;
+
+namespace SharedKernel.SeedWork
+{
+    public interface IRepository<T, TId> where T : AggregateRoot<TId> where TId : notnull
+    {
+        Task<T?> GetByIdAsync(TId id, CancellationToken ct = default);
+        Task<IReadOnlyList<T>> GetListAsync(CancellationToken ct = default);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<Result> CommitAsync(CancellationToken ct = default);
+    }
+}
