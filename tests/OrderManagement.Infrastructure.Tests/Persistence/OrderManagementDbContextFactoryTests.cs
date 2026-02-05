e21ca0668454dfc4b5e6937aa7083d27a2901bce
dominicgrob123 <dominic.grob@hotmail.ch>
2026-02-05 13:39:41 +0100

    added user-secrets and tests for DbContext + Factory

---
+++ tests/OrderManagement.Infrastructure.Tests/Persistence/OrderManagementDbContextFactoryTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Configuration;
+
+using OrderManagement.Infrastructure.Persistence;
+
+namespace OrderManagement.Infrastructure.Tests.Persistence
+{
+    [TestClass]
+    public sealed class OrderManagementDbContextFactoryTests
+    {
+        [TestMethod]
+        public void CreateDbContextThrowsWhenConnectionStringMissing()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().Build();
+            var factory = new OrderManagementDbContextFactory(config);
+
+            _ = Assert.ThrowsException<InvalidOperationException>(
+                () => factory.CreateDbContext([]));
+        }
+
+        [TestMethod]
+        public void CreateDbContextUsesProvidedConnectionString()
+        {
+            KeyValuePair<string, string>[] initialData =
+[
+    new KeyValuePair<string, string>(
+        "ConnectionStrings:OrderManagement",
+        "Server=(localdb)\\mssqllocaldb;Database=OrderManagement.Tests;Trusted_Connection=True;")
+];
+
+            IConfigurationRoot inMemoryConfig = new ConfigurationBuilder()
+                .AddInMemoryCollection(initialData)
+                .Build();
+
+
+            var factory = new OrderManagementDbContextFactory(inMemoryConfig);
+
+            using OrderManagementDbContext context = factory.CreateDbContext([]);
+
+        }
+
+
+    }
+}
