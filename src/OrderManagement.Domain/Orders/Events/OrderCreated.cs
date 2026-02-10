109a2910472fefd8857571ad5b294f3747684bff
LordofBomb <fabian.ledergerber@bluewin.ch>
2026-02-10 18:06:33 +0100

    Fehler in FeatureOrder behoben

--- src/OrderManagement.Domain/Orders/Events/OrderCreated.cs
+++ src/OrderManagement.Domain/Orders/Events/OrderCreated.cs
@@ -1,9 +1,9 @@
 using OrderManagement.Domain.Orders.ValueObjects;
 
-using SharedKernel.SeedWork; // Wichtig: Hier liegt DomainEvent laut deiner Info
+using SharedKernel.SeedWork;
 
 namespace OrderManagement.Domain.Orders.Events;
 
-// Wir erben vom abstrakten record und reichen das Datum an die Basisklasse weiter
+
 public record OrderCreated(OrderId OrderId, DateTime OccurredOnUtc)
     : DomainEvent(OccurredOnUtc);
