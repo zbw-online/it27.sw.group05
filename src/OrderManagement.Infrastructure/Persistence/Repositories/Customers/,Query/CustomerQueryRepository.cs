142d471ab4dcb420a18b199e9b95104315635d2f
dominicgrob123 <dominic.grob@hotmail.ch>
2026-02-15 10:07:48 +0100

    formatting

--- src/OrderManagement.Infrastructure/Persistence/Repositories/Customers/,Query/CustomerQueryRepository.cs
+++ src/OrderManagement.Infrastructure/Persistence/Repositories/Customers/,Query/CustomerQueryRepository.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace OrderManagement.Infrastructure.Persistence.Repositories.Customers.Query
 {
-    internal class CustomerQueryRepository
+    internal sealed class CustomerQueryRepository
     {
     }
 }
--- tests/OrderManagement.Infrastructure.Tests/Persistence/Repositories/Customers/Query/CustomerQueryRepositoryTests.cs
+++ tests/OrderManagement.Infrastructure.Tests/Persistence/Repositories/Customers/Query/CustomerQueryRepositoryTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace OrderManagement.Infrastructure.Tests.Persistence.Repositories.Customers.Query
 {
-    internal class CustomerQueryRepositoryTests
+    internal sealed class CustomerQueryRepositoryTests
     {
     }
 }
